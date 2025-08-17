f38ccbfbd5d5b6a5f1642ff41ad38a9cdee0424a
sec2john <sec2john@kali.kali.eu>
2025-08-17 18:10:55 +0200

    +1 Modulo_1 1.1

---
+++ Modulo_1/1.1_HelloWorld.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+#       ____        ___     __     __
+#      / __/__ ____|_  |__ / /__  / /  ___
+#     _\ \/ -_) __/ __// // / _ \/ _ \/ _
+#    /___/\__/\__/____/\___/\___/_//_/_//_/
+#
+# Modulo 1
+# Lección 1.1 ¿Qué es un script en Bash?
+#
+# https://www.sec2john.com
+#
+
+echo "Hello World!"
+
