52c5e104c0099c77eb587936f93ea51e2942c9b3
sec2john <sec2john@gmail.com>
2025-10-02 18:42:23 +0200

    BSC+1 fix 2.1_Parameter_Expansion.sh

--- Modulo_2/2.1_Parameter_Expansion.sh
+++ Modulo_2/2.1_Parameter_Expansion.sh
@@ -13,7 +13,7 @@
 #
 
 #Si el parámetro $1 es nulo escribir un warning por pantalla
-${1:-"El parámetro 1 está vacío o no está definido."}
+echo ${1:-"El parámetro 1 está vacío o no está definido."}
 
 #Si el parámetro $1 es nulo asignar "John Doe" a la variable name
 var1=$1
@@ -24,7 +24,7 @@ var2=$2
 #Mostrar un substring del parámetro 2 desde la posición 3 hasta la posición 12
 echo "${var2:3:12}"
 
-#sustituir el caracter '3' por '6' una vez, y de forma global del parametro $3
+#sustituir el caracter '5' por '6' una vez, y de forma global del parametro $3
 var3=$3
 echo ${var3/5/6} 
 echo ${var3//5/6}
